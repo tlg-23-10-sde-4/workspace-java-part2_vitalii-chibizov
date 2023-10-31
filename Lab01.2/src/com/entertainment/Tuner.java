8295351dfbe744c60218e5287c001af1ae182467
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-10-31 12:51:26 -0700

    Lab01.2 (Classes and Objects) - complete

--- Lab01.2/src/com/entertainment/Tuner.java
+++ Lab01.2/src/com/entertainment/Tuner.java
@@ -8,13 +8,14 @@
  */
 package com.entertainment;
 
-public class Tuner {
-  private int channel = 3;  // default channel for cable and satellite customers
-  
-  public int getChannel() {
-    return this.channel;
-  }
-  public void setChannel(int channel) {
-    this.channel = channel;
-  } 
+ class Tuner {
+    private int channel = 3;  // default channel for cable and satellite customers
+
+    public int getChannel() {
+        return this.channel;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
 }
