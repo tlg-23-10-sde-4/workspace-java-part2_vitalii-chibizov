f5a860bbe2b5530caedffdfc2628329df4d06073
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-11-03 15:38:31 -0700

    Lab04.1 (Catalog of Television) - complete

--- Lab04.1/src/com/entertainment/catalog/Catalog.java
+++ Lab04.1/src/com/entertainment/catalog/Catalog.java
@@ -16,49 +16,36 @@ import java.util.Map;
 import com.entertainment.Television;
 
 public class Catalog {
-  // in-memory catalog of Televisions
   private static final Collection<Television> catalog = new ArrayList<>(30);
 
-  // prevent direct instantiation, this is an all-static class
   private Catalog() {
   }
-  
-  /**
-   * Searches catalog by brand, and returns a collection of matching Televisions.
-   * A no-matches result should be an empty collection (not null).
-   */
+
   public static Collection<Television> findByBrand(String brand) {
-    return null;
+    Collection<Television> result = new ArrayList<>();
+    for (Television tv : catalog) {
+      if (tv.getBrand().equals(brand)) {
+        result.add(tv);
+      }
+    }
+    return result;
   }
-  
-  /**
-   * Searches catalog by one or more brands, and returns a map with an entry for each brand supplied, 
-   * with a corresponding collection of matching Televisions for that brand.
-   * A no-brands-passed result should be an empty map (not null).
-   */
+
   public static Map<String,Collection<Television>> findByBrands(String... brands) {
-    return null;
+    Map<String,Collection<Television>> result = new HashMap<>();
+
+    for (String brand : brands) {
+      Collection<Television> tvs = findByBrand(brand);
+      result.put(brand, tvs);
+    }
+    return result;
   }
 
-  /**
-   * Returns entire catalog.
-   * NOTE: returning a direct reference to it has consequences!
-   *  A client can manipulate it, since it has a direct reference to it.
-   *  Sometimes this is okay, but not here.
-   *  
-   * TODO: change this to return a read-only view of the catalog.
-   * 
-   * You should explore the Javadoc for the java.util.Collections *class*.
-   *  This is an all-static utility class, not the java.util.Collection interface.
-   */
   public static Collection<Television> getInventory() {
-    return catalog;
+    //  return catalog;
+    return Collections.unmodifiableCollection(catalog);
   }
 
-  /*
-   * Loads catalog.
-   * Static initializers execute when the class is loaded into the JVM.
-   */
   static {
     catalog.add(new Television("Zenith",  0));
     catalog.add(new Television("Sony",    4));
@@ -69,10 +56,10 @@ public class Catalog {
     catalog.add(new Television("Hitachi", 13));
     catalog.add(new Television("Hitachi", 40));
     catalog.add(new Television("Zenith",  46));
-    catalog.add(new Television("RCA",     50));
-    catalog.add(new Television("Sony",    94));
-    catalog.add(new Television("RCA",     50));
-    catalog.add(new Television("Sony",    50));
+    catalog.add(new Television("RCA",     50)); //  dup
+    catalog.add(new Television("Sony",    94)); //  the loudest
+    catalog.add(new Television("RCA",     50)); //  dup
+    catalog.add(new Television("Sony",    50)); //  dup
     catalog.add(new Television("Hitachi", 50));
     catalog.add(new Television("Zenith",  37));
     catalog.add(new Television("RCA",     31));
@@ -81,14 +68,14 @@ public class Catalog {
     catalog.add(new Television("Zenith",  27));
     catalog.add(new Television("Zenith",  12));
     catalog.add(new Television("RCA",     10));
-    catalog.add(new Television("Hitachi", 50));
+    catalog.add(new Television("Hitachi", 50)); //  dup
     catalog.add(new Television("RCA",     4));
-    catalog.add(new Television("RCA",     50));
-    catalog.add(new Television("RCA",     50));
+    catalog.add(new Television("RCA",     50)); //  dup
+    catalog.add(new Television("RCA",     50)); //  dup
     catalog.add(new Television("Sony",    28));
     catalog.add(new Television("Zenith",  50));
     catalog.add(new Television("Zenith",  77));
     catalog.add(new Television("Sony",    22));
-    catalog.add(new Television("RCA",     50));
+    catalog.add(new Television("RCA",     50)); //  dup
   }
 }
--- Lab04.1/test/com/entertainment/catalog/CatalogTest.java
+++ Lab04.1/test/com/entertainment/catalog/CatalogTest.java
@@ -1,27 +1,108 @@
-/*
- * This code is sample code, provided as-is, and we make no
- * warranties as to its correctness or suitability for
- * any purpose.
- *
- * We hope that it's useful to you.  Enjoy.
- * Copyright LearningPatterns Inc.
- */
 package com.entertainment.catalog;
 
 import static org.junit.Assert.*;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
 import com.entertainment.Television;
 
 public class CatalogTest {
-  
-  /**
-   * Contract: a no-matches result should be an empty collection (not null).
-   */
-  @Test
-  public void testFindByBrandNoMatches() {
-    Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
-    assertNotNull(tvs);
-    assertTrue(tvs.isEmpty());
-  }
+
+    //  single brand
+    @Test
+    public void testFindByBrand() {
+        Collection<Television> tvs = Catalog.findByBrand("Sony");
+        for (Television tv : tvs) {
+            assertEquals("Sony", tv.getBrand());
+        }
+    }
+
+    @Test
+    public void testFindByBrand_returnsTrue_whenBrandNotFound() {
+        Collection<Television> tvs = Catalog.findByBrand("WrongBrand");
+        assertNotNull(tvs);
+        assertTrue(tvs.isEmpty());  //  check isEmpty -> it IS empty
+    }
+
+    @Test
+    public void testFindByBrands_returnsTrue_whenWrongBrandInput() {
+        Map<String, Collection<Television>> tvMap = Catalog.findByBrands("WrongBrand");
+        assertNotNull(tvMap); //  check it exists
+        assertFalse(tvMap.isEmpty());  //  check isEmpty -> it IS NOT empty
+        assertEquals(0, tvMap.get("WrongBrand").size());
+    }
+
+    @Test
+    public void testFindByBrands_returnsTrue_ifNumberOfMatchesMatch() {
+        Map<String, Collection<Television>> tvMap = Catalog.findByBrands("Sony", "Zenith", "NO-MATCHES");
+        assertEquals(7, tvMap.get("Sony").size());
+        assertEquals(9, tvMap.get("Zenith").size());
+        assertEquals(0, tvMap.get("NO-MATCHES").size());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testGetInventory_shouldThrowException_dueToReadOnly() {
+        Collection<Television> inventory = Catalog.getInventory();
+        inventory.removeAll(inventory);
+    }
+
+    @Test
+    public void testRemoveDuplicates_returnsTrue_ifSizeOfHashSetMatch(){
+        Collection<Television> inventory = Catalog.getInventory();
+        assertEquals(30,inventory.size());
+
+        Set<Television> uniqueInventory = new HashSet<>(inventory);
+        assertEquals(23, uniqueInventory.size());   //  counted in catalog
+    }
+
+    @Test
+    public void testLoudest_returnsTrueIfLoudestTV_match(){
+        Set<Television> uniqueInventory = new HashSet<>(Catalog.getInventory());
+        Television theLoudest = new Television();
+
+        for (Television tv : uniqueInventory){
+            if (tv.getVolume() > theLoudest.getVolume()){
+                theLoudest = tv;
+            }
+        }
+        assertEquals("Sony", theLoudest.getBrand());
+        assertEquals(94, theLoudest.getVolume());
+    }
+
 }
+/*
+ catalog.add(new Television("Zenith",  0));
+             catalog.add(new Television("Zenith",  0));
+    catalog.add(new Television("Sony",    4));
+    catalog.add(new Television("Sony",    50));
+    catalog.add(new Television("Zenith",  33));
+    catalog.add(new Television("RCA",     50));
+    catalog.add(new Television("Zenith",  86));
+    catalog.add(new Television("Hitachi", 13));
+    catalog.add(new Television("Hitachi", 40));
+    catalog.add(new Television("Zenith",  46));
+    catalog.add(new Television("RCA",     50)); //  dup
+    catalog.add(new Television("Sony",    94)); //  the loudest
+    catalog.add(new Television("RCA",     50)); //  dup
+    catalog.add(new Television("Sony",    50)); //  dup
+    catalog.add(new Television("Hitachi", 50));
+    catalog.add(new Television("Zenith",  37));
+    catalog.add(new Television("RCA",     31));
+    catalog.add(new Television("Sony",    87));
+    catalog.add(new Television("Hitachi", 39));
+    catalog.add(new Television("Zenith",  27));
+    catalog.add(new Television("Zenith",  12));
+    catalog.add(new Television("RCA",     10));
+    catalog.add(new Television("Hitachi", 50)); //  dup
+    catalog.add(new Television("RCA",     4));
+    catalog.add(new Television("RCA",     50)); //  dup
+    catalog.add(new Television("RCA",     50)); //  dup
+    catalog.add(new Television("Sony",    28));
+    catalog.add(new Television("Zenith",  50));
+    catalog.add(new Television("Zenith",  77));
+    catalog.add(new Television("Sony",    22));
+    catalog.add(new Television("RCA",     50)); //  dup
+ */
