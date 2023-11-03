b3b730a98a3203bb524fba32ea72c68125af71a6
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-11-03 15:39:49 -0700

    Lab04.1 Extra (Collection of music items) complete

---
+++ Lab04.1 Extra/test/com/javatunes/catalog/CatalogTest.java
@@ -0,0 +1,14 @@
+package com.javatunes.catalog;
+
+import static org.junit.Assert.*;
+
+public class CatalogTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void findByKeyword() {
+    }
+}
