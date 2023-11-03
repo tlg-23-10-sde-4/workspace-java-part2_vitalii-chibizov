ffa3760c6e6814efcb9fd87c4b397cac6609e5cd
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-11-03 15:38:04 -0700

    Lab03.1  complete

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,47 @@
 package com.math;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    //  fixture: business object(s) under test
+    private Calculator calc;
+
+    @BeforeClass
+    public static void initializeTestRun(){
+        System.out.println("finalizeTetRun");
+    }
+
+    @Before
+    public void setUp(){
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+
+    @After
+    //  another name - tearDown
+    public void cleanUp(){
+        System.out.println("cleanUp");
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
+    @Test
+    public void testDivide() {
+        double result = calc.divide(5, 2);
+        assertEquals(2.5, result, .001);
+    }
+
+    @Test
+    public void testIsEven() {
+        assertTrue(calc.isEven(10));
+    }
 }
